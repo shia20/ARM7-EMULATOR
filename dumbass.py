d41ded54acb0229ff2154a29ae5db1eea70e3970
Rishit <puligaddarishit@gmail.com>
2024-12-21 20:28:58 +0530

    added the test file

--- dumbass.py
+++ dumbass.py
@@ -1,7 +1,9 @@
 def read_file():
+    code = []
     with open("test.asm", 'r') as f:
         for line in f:
-            print(line, end="")
+            code.append(line)
+    print(code)
 
 
 if __name__ == "__main__":
